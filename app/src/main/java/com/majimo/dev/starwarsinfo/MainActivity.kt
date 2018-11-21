34b514382c1b04fbc4d0faaf9039e5b6674f07ed
Pierre FERVEL <pierre.fervel@gmail.com>
2018-11-21 19:42:53 +0100

    Inserted few data treatments on planet result

--- app/src/main/java/com/majimo/dev/starwarsinfo/MainActivity.kt
+++ app/src/main/java/com/majimo/dev/starwarsinfo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.majimo.dev.starwarsinfo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import com.majimo.dev.starwarsinfo.repository.PlanetRepository
 import com.majimo.dev.starwarsinfo.service.PlanetService
 import retrofit2.Call
@@ -12,10 +13,18 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var planet: PlanetRepository
+    private lateinit var planetName: TextView
+    private lateinit var planetClimate: TextView
+    private lateinit var planetTerrain: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        planetName = findViewById(R.id.tv_planet_name)
+        planetClimate = findViewById(R.id.tv_planet_climate2)
+        planetTerrain = findViewById(R.id.tv_planet_terrain2)
 
         val service = Retrofit.Builder()
             .baseUrl("https://swapi.co/api/")
@@ -35,8 +44,16 @@ class MainActivity : AppCompatActivity() {
                     response: Response<PlanetRepository>
                 ) {
                     Log.wtf("XXX", "Planete : " + response.body())
-                }
+                    if (response.body() != null) run {
+                        planet = response.body()!!
 
+                        if (planet != null) {
+                            planetName.setText(planet.name)
+                            planetClimate.setText(planet.climate)
+                            planetTerrain.setText(planet.terrain)
+                        }
+                    }
+                }
             })
     }
 }
